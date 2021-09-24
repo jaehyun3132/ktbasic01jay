a7dcb14d71045af0acb85e63cb74f221d5d41aaa
jaehyun <wogus3132@naver.com>
2021-09-24 18:26:15 +0900

    로그 찍어보기

--- app/src/main/java/com/example/ktbasic01jay/MainActivity.kt
+++ app/src/main/java/com/example/ktbasic01jay/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.ktbasic01jay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
-        smallBtn
+        clickBtn.setOnClickListener {
+
+            //코드에 메모 남기기 + 주석
+// Ctrl + / 하면 주석처리 {이 중괄호} 안에 있는 코드만 ,버튼이 눌렸을 떄 실행됨
+
+            Log.d("메인화면로그", "클릭버튼이 눌림")
+
+        }
     }
 }
