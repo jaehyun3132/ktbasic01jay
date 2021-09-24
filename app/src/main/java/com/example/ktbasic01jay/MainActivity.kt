6fcbc2bb2c89503c7fca3ad941d60b2e4db7da53
jaehyun <wogus3132@naver.com>
2021-09-24 18:41:12 +0900

    로그사용 d. 디버깅 e 에러관련 토스트 사용해보기

--- app/src/main/java/com/example/ktbasic01jay/MainActivity.kt
+++ app/src/main/java/com/example/ktbasic01jay/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ktbasic01jay
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,10 @@ class MainActivity : AppCompatActivity() {
 // Ctrl + / 하면 주석처리 {이 중괄호} 안에 있는 코드만 ,버튼이 눌렸을 떄 실행됨
 
             Log.d("메인화면로그", "클릭버튼이 눌림")
+            Log.e("메인화면로그","에러관련로그")
+        }
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼 눌림", Toast.LENGTH_SHORT).show()
 
         }
     }
